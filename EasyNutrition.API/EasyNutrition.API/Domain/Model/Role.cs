5ea91dbd601111bd4ef93804dd1d48d3c6da857b
Dino Iván Pérez Vásquez <55163683+Dinoelfantastico@users.noreply.github.com>
2022-06-01 16:13:37 -0500

    Trabajo
    Clases Role y User

---
+++ EasyNutrition.API/EasyNutrition.API/Domain/Model/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyNutrition.API.Domain.Model
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<User> Users { get; set; } = new List<User>();
+
+    }
+}
---
+++ EasyNutrition.API/EasyNutrition.API/Domain/Model/User.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyNutrition.API.Domain.Model
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Birthday { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+        public bool Active { get; set; }
+        public string Linkedin { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
